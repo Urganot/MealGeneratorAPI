435941cd6a646b8899a9b5a4bbe866638299d939
Urganot <ballingsimon@googlemail.com>
2024-07-22 13:13:49 +0200

    Added meal model

--- meals_api/models.py
+++ meals_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+    name = models.CharField(max_length=255)
+    chefkoch_slug = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_chefkoch_url(self):
+        return "https://www.chefkoch.de/rezepte/" + self.chefkoch_slug
